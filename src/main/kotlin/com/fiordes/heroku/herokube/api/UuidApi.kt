362570951fa16ed89330a7a9415ff64adce2082f
fiordy <fiordes.agaj@gmail.com>
2021-09-26 02:32:34 +0200

    generate uuid api

--- src/main/kotlin/com/fiordes/heroku/herokube/api/UuidApi.kt
+++ src/main/kotlin/com/fiordes/heroku/herokube/api/UuidApi.kt
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.RequestMethod
 interface UuidApi {
     /**
      * Generates a new UUID
+     * @return uuid string
      */
     @RequestMapping(
         path = ["/generate"],
